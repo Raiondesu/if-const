dddbf5c9f43ec46f5a162b5427033eb25f940377
Raiondesu <a.raiondesu@gmail.com>
2021-02-15 13:57:30 +0300

    Clean dist

--- dist/es/index.js
+++
@@ -1,7 +0,0 @@
-import { ifConst } from "./if-const.js";
-export * from "./if-const.js";
-export default ifConst;
-export function constIf(f, elf) {
-    return cond => ifConst(cond, f, elf);
-}
-//# sourceMappingURL=index.js.map
